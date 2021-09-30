c2f056fc06516f09d8882bf6a65fb4d415dbdd7a
Rody Molenaar <rody@lightbase.nl>
2021-09-30 14:20:42 +0200

    feat: update eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,10 @@
-module.exports = require("@lightbase/eslint-config-lightbase/prettierrc");
+module.exports = {
+  printWidth: 110,
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSpacing: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  semi: true,
+  proseWrap: "always",
+};
