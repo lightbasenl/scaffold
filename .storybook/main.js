3344dbcde5eb40b27afc1c7b5906c06f2d7c1e41
Rody Molenaar <rody@lightbase.nl>
2021-12-30 00:16:52 +0100

    feat: upgrade Storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../stories/*.story.@(ts|tsx|js|jsx|mdx)"],
+  stories: ["../stories/**/*.story.@(ts|tsx|js|jsx|mdx)"],
   addons: ["@storybook/addon-links", '@storybook/addon-docs', "@storybook/addon-essentials", {
     name: "@storybook/addon-postcss",
     options: {
@@ -12,12 +12,22 @@ module.exports = {
       },
     }
   }],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
   webpackFinal: async (config) => {
     config.resolve.modules = [path.resolve(__dirname, "../src"), "node_modules"];
 
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+      }
+    };
+
     return config;
-  },
-  typescript: {
-    reactDocgen: 'none', // temporary work-around for TypeScript 4.3.x, see: https://github.com/styleguidist/react-docgen-typescript/issues/356
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,17 @@
 import "../src/css/tailwind.css";
-import { withNextRouter } from 'storybook-addon-next-router';
-import {addDecorator} from "@storybook/react";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
-  options: {
-    storySort: (a, b) => {
-      // We want the Welcome story at the top
-      if (b[1].kind === "Introduction") {
-        return 1;
-      }
-
-      // Sort the other stories by ID
-      // https://github.com/storybookjs/storybook/issues/548#issuecomment-530305279
-      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true });
-    },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 };
-
-addDecorator(
-  withNextRouter({
-    path: '/', // defaults to `/`
-    asPath: '/', // defaults to `/`
-    query: {}, // defaults to `{}`
-    push() {}, // defaults to using addon actions integration, can override any method in the router
-    locale: 'en'
-  })
-);
