ede164441c5475c1c677ed456123e75165893274
Rody Molenaar <rody@lightbase.nl>
2022-01-10 09:31:06 +0100

    fix: support baseUrl in Jest

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
   testEnvironment: "jest-environment-jsdom",
 };
 
