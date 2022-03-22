6ad8e5388ef2ec31ec57a539747b428910bb40e0
Rody Molenaar <rody@lightbase.nl>
2022-03-22 12:25:15 +0100

    fix: ignore e2e tests in jest config

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/src"],
   testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["/e2e/"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
