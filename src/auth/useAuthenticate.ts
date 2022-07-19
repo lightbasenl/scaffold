5513cc8581edbaca60a4c1ac0b53220caa576df8
Dirk de Visser <github@dirkdevisser.nl>
2022-07-19 12:20:14 +0200

    feat(auth): add login and session type check support

--- src/auth/useAuthenticate.ts
+++ src/auth/useAuthenticate.ts
@@ -3,16 +3,50 @@ import { useCallback, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { useAuthMe } from "generated/auth/reactQueries";
-import type { AuthPermissionIdentifierApi } from "generated/common/types";
+import type { AuthPermissionIdentifierApi, AuthSessionApi } from "generated/common/types";
 
+/**
+ * Expect a response on {@see useAuthMe}. Else redirects to `/`.
+ *
+ * This hook is also able to execute a some checks regarding the return user and session.
+ *
+ * @example
+ * It can require a specific session type, enforcing that the user is past the `checkTwoStep`
+ *   stage
+ * ```ts
+ *   useAuthenticate({ enforceSessionType: "user" });
+ * ```
+ *
+ * Or that the session is created via a specific authentication method:
+ * ```ts
+ *   useAuthenticate({ enforceLoginType: "anonymousBased" });
+ * ```
+ *
+ * It can also check if the user has the required permissions;
+ * ```ts
+ *   // Require all the provided permissions
+ *   useAuthenticate({ requireAllPermissions: ["scaffold:dashboard:manage",
+ *   "scaffold:dashboard:use"] })
+ *   // Require one of the provided permissions
+ *   useAuthenticate({ requireSinglePermission: ["scaffold:dashboard:manage",
+ *   "scaffold:dashboard:use"] })
+ * ```
+ *
+ * All above checks can be combined
+ */
 export default function useAuthenticate({
+  enforceSessionType,
+  enforceLoginType,
   requireAllPermissions,
-  requireSinglePermission,
+  requireSinglePermission
 }: {
-  requireAllPermissions?: AuthPermissionIdentifierApi[];
-  requireSinglePermission?: AuthPermissionIdentifierApi[];
+  enforceLoginType?: AuthSessionApi["loginType"]; enforceSessionType?: AuthSessionApi["type"]; requireAllPermissions?: AuthPermissionIdentifierApi[]; requireSinglePermission?: AuthPermissionIdentifierApi[];
 } = {}) {
-  const { data, isLoading, isLoadingError } = useAuthMe();
+  const {
+    data,
+    isLoading,
+    isLoadingError
+  } = useAuthMe();
   const router = useRouter();
 
   const routerPush = router.push;
@@ -23,8 +57,8 @@ export default function useAuthenticate({
     routerPush({
       pathname: "/",
       query: {
-        from: asPath,
-      },
+        from: asPath
+      }
     });
   }, [asPath, routerPush]);
 
@@ -37,6 +71,22 @@ export default function useAuthenticate({
 
     redirectToLogin();
   }, [redirectToLogin, isLoading, isLoadingError]);
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    if (enforceSessionType && enforceSessionType !== data.session.type) {
+      redirectToLogin();
+      return;
+    }
+
+    if (enforceLoginType && enforceLoginType !== data.session.loginType) {
+      redirectToLogin();
+      return;
+    }
+  }, [data, enforceLoginType, enforceSessionType, redirectToLogin]);
 
   useEffect(() => {
     if (!data) {
