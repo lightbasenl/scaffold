55b0e18387ac201104f713271622be0cd9eee416
Rody Molenaar <rody@lightbase.nl>
2022-08-18 13:04:18 +0200

    feat: csp

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,6 +1,5 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
-
 import _Document, { Head, Html, Main, NextScript } from "next/document";
 
 class Document extends _Document {
